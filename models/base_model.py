f7b9152cbd02ce80f4f6d5a65a0eedb2e851b437
ilyasouagag <ilyasouagag2011@gmail.com>
2024-01-08 19:47:25 +0100

    kwargs added

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,21 @@ import uuid
 from datetime import datetime
 
 class BaseModel():
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+        if kwargs is not None:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key,value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.__dict__[key] = datetime.strptime(value, date_format)
+                elif key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(value, date_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -25,3 +35,24 @@ class BaseModel():
         dic['updated_at'] = updated_at_iso
         return dic
 
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
