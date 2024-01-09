0e983cb7d61dee79df0768bc656cd7faa76c8859
Alaa Eddine <Alaaelhannaoui@proton.me>
2024-01-09 18:22:59 +0100

    first consol done

--- console.py
+++ console.py
@@ -4,15 +4,22 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     
-    def do_help(self, arg: str) -> bool | None:
+
+    def do_help(self, arg):
+        """This is the help command"""
         return super().do_help(arg)
-    
 
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
 
+    def do_EOF(self, arg):
+        """Exit the program on EOF (Ctrl+D)"""
+        return True
 
-
-
-
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
 
 
 
